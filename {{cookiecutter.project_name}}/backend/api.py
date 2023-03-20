d4adb7d08d4de968dfd3e24d88742fdd5291bda6
Edward Gorman <ejgorman@gmail.com>
2023-03-20 17:40:45 +0000

    Minor fixes to dockerfiles

--- {{cookiecutter.project_name}}/backend/api.py
+++ {{cookiecutter.project_name}}/backend/api.py
@@ -4,7 +4,7 @@ import logging
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-import database
+import database  # Required to initialise the connection to the database
 from backend.routers.fruit import router as fruit_router
 
 
